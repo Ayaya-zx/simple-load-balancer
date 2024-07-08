4797e9cb8b1696f978e88e1519a09e1e9632bff4
Ayaya-zx <aagranov7@gmail.com>
2024-07-08 18:32:53 +0300

    Added viper configuration

--- main.go
+++ main.go
@@ -1,50 +1,62 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
 
 	"github.com/Ayaya-zx/simple-load-balancer/slb"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 type ServerConf struct {
-	URL       *url.URL `json:"url"`
-	ReadyPath string   `json:"ready"`
+	URL   *url.URL `json:"url"`
+	Ready string   `json:"ready"`
 }
 
 func main() {
-	port := flag.Int("p", 8765, "Port")
+	var port int
+	v := viper.New()
 
-	data, err := os.ReadFile("servers.json")
+	v.SetDefault("Port", 8765)
+	v.SetDefault("Host", "localhost")
+
+	v.AutomaticEnv()
+	v.SetEnvPrefix("SLB")
+	v.BindEnv("Port", "port")
+
+	pflag.IntVarP(&port, "port", "p", 8765, "Port")
+	pflag.Parse()
+	if err := v.BindPFlag("Port", pflag.Lookup("port")); err != nil {
+		log.Fatalln(err)
+	}
+
+	v.SetConfigName("conf")
+	v.SetConfigType("json")
+	v.AddConfigPath(".")
+	err := v.ReadInConfig()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var serverConf []*ServerConf
+	err = v.UnmarshalKey("Servers", &serverConf)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var serversConf []ServerConf
-	err = json.Unmarshal(data, &serversConf)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	servers := make([]*slb.Server, 0, len(serversConf))
-	for _, conf := range serversConf {
-		if conf.URL == nil {
-			log.Fatal("Errors in servers.json")
-		}
+	servers := make([]*slb.Server, 0, len(serverConf))
+	for _, conf := range serverConf {
 		servers = append(servers, slb.NewServer(
-			conf.URL, slb.WithReadyCheck(conf.ReadyPath),
-		))
+			conf.URL, slb.WithReadyCheck(conf.Ready)))
 	}
 
 	b := slb.NewBalancer(servers)
 	http.HandleFunc("/", b.Handle)
-	err = http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", v.GetInt("Port")), nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 }
